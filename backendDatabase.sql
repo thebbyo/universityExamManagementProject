d4657adf23e383e84516b6e87f2eb5aff0213757
Diponker Roy Dibbyo <115203982+thebbyo@users.noreply.github.com>
2023-09-17 09:54:40 +0600

    Create backendDatabase.sql

---
+++ backendDatabase.sql
@@ -0,0 +1,21 @@
+create table students(
+  studentID int primary key,
+  studentName varchar(255) not null
+);
+create table courses(
+  courseID int primary key,
+  courseName varchar(255) not null,
+  courseCredit double not null
+);
+create table exams(
+  examID int primary key,
+  studentID int,
+  courseID int,
+  incourseMarks double,
+  finalMarks double,
+  totalMarks double,
+  isRetake boolean default false,
+  isImprovement boolean default false,
+  foreign key(studentID) references students(studentID),
+  foreign key(courseID) references courses(courseID)
+);
